53cf0059ded022ef5c623958a1d9e4509b569599
Socrates Palma <dubstepero10@gmail.com>
2022-01-28 22:33:48 -0600

    navbar fully working on mobile

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-// import { BiMenu } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-import MenuButton from './MenuButton';
 import Navbar from './Navbar';
 
 export default function Header() {
@@ -10,9 +8,7 @@ export default function Header() {
       <Link to="/" className="logo">
         SPG
       </Link>
-
       <Navbar />
-      <MenuButton />
     </header>
   );
 }
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,31 +1,61 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { FiMenu, FiX } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef(null);
+  const showNavRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      navRef.current.style.visibility = 'visible';
+      showNavRef.current.style.transform = 'translateY(0)';
+    } else {
+      navRef.current.style.visibility = 'hidden';
+      showNavRef.current.style.transform = 'translateY(-100%)';
+    }
+  }, [isOpen]);
+
   return (
-    <nav className="nav">
-      <ul className="menu-nav">
-        <li className="menu-nav__item">
-          <NavLink className="menu-nav__link" to="/">
-            Home
-          </NavLink>
-        </li>
-        <li className="menu-nav__item">
-          <NavLink className="menu-nav__link" to="/">
-            Services
-          </NavLink>
-        </li>
-        <li className="menu-nav__item">
-          <NavLink className="menu-nav__link" to="/">
-            About
-          </NavLink>
-        </li>
-        <li className="menu-nav__item">
-          <NavLink className="menu-nav__link" to="/">
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="nav" ref={navRef}>
+        <ul className="menu-nav" ref={showNavRef}>
+          <li className="menu-nav__item">
+            <NavLink
+              className="menu-nav__link"
+              to="/"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="menu-nav__item">
+            <NavLink className="menu-nav__link" to="/">
+              Services
+            </NavLink>
+          </li>
+          <li className="menu-nav__item">
+            <NavLink className="menu-nav__link" to="/">
+              About
+            </NavLink>
+          </li>
+          <li className="menu-nav__item">
+            <NavLink className="menu-nav__link" to="/">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <button
+        type="button"
+        className="menu-btn"
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        {isOpen ? <FiX /> : <FiMenu />}
+      </button>
+    </>
   );
 }
